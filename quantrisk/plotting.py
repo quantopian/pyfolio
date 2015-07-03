90cf7b5bfa5a28997b5b4b392aff69d3416c5fc7
Thomas Wiecki <thomas.wiecki@gmail.com>
2015-07-03 20:23:18 +0200

    BUG Attempt to fix boxplot bug. Apparently API in seaborn 0.6 changed.

--- quantrisk/plotting.py
+++ quantrisk/plotting.py
@@ -573,8 +573,8 @@ def plot_return_quantiles(df_rets, df_weekly, df_monthly, ax=None, **kwargs):
     if ax is None:
         ax = plt.gca()
 
-    sns.boxplot([df_rets, df_weekly, df_monthly],
-                names=['daily', 'weekly', 'monthly'],
+    sns.boxplot(x=['daily', 'weekly', 'monthly'],
+                y=[df_rets, df_weekly, df_monthly],
                 ax=ax, **kwargs)
     ax.set_title('Return quantiles')
     return ax
