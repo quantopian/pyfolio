8abb9c2bbf6eb7c85266549c37fade464bffc4db
Thomas Wiecki <thomas.wiecki@gmail.com>
2015-07-09 12:12:33 +0200

    DOC Add docstrings for bayesian functions.

--- quantrisk/bayesian.py
+++ quantrisk/bayesian.py
@@ -27,6 +27,33 @@ import pymc3 as pm
 from .timeseries import cum_returns
 
 def model_returns_t_alpha_beta(data, bmark, samples=2000):
+    """Run Bayesian alpha-beta-model with T distributed returns.
+
+    This model estimates intercept (alpha) and slope (beta) of two
+    return sets. Usually, these will be algorithm returns and
+    benchmark returns (e.g. S&P500). The data is assumed to be T
+    distributed and thus is robust to outliers and takes tail events
+    into account.
+
+    Parameters
+    ----------
+    df_rets : pandas.Series
+        Series of simple returns of an algorithm or stock.
+    bmark : pandas.Series
+        Series of simple returns of a benchmark like the S&P500.
+        If bmark has more recent returns than df_train, these dates
+        will be treated as missing values and predictions will be
+        generated for them taking market correlations into account.
+    samples : int (optional)
+        Number of posterior samples to draw.
+
+    Returns
+    -------
+    pymc3.sampling.BaseTrace object
+        A PyMC3 trace object that contains samples for each parameter
+        of the posterior.
+    """
+
     if len(data) != len(bmark):
         # pad missing data
         data = pd.Series(data, index=bmark.index)
@@ -55,7 +82,33 @@ def model_returns_t_alpha_beta(data, bmark, samples=2000):
 
     return trace
 
+
 def model_returns_normal(data, samples=500):
+    """Run Bayesian model assuming returns are Student-T distributed.
+
+    Compared with the normal model, this model assumes returns be
+    T-distributed and thus has a 3rd parameter (nu) that controls the
+    mass in the tails.
+
+    In addition to these parameters, the annual Sharpe ratio is
+    computed by dividing the mean by the standard deviation of the
+    returns distribution; and the annual volatility is computed by
+    computing the standard deviation of the returns distribution.
+
+    Parameters
+    ----------
+    df_rets : pandas.Series
+        Series of simple returns of an algorithm or stock.
+    samples : int (optional)
+        Number of posterior samples to draw.
+
+    Returns
+    -------
+    pymc3.sampling.BaseTrace object
+        A PyMC3 trace object that contains samples for each parameter
+        of the posterior.
+
+    """
     with pm.Model() as model:
         mu = pm.Normal('mean returns', mu=0, sd=.01, testval=data.mean())
         sigma = pm.HalfCauchy('volatility', beta=1, testval=data.std())
@@ -70,7 +123,30 @@ def model_returns_normal(data, samples=500):
         trace = pm.sample(samples, step, start=start)
     return trace
 
+
 def model_returns_t(data, samples=500):
+    """Run Bayesian model assuming returns are normally distributed.
+
+    In addition to these parameters, the annual Sharpe ratio is
+    computed by dividing the mean by the standard deviation of the
+    returns distribution; and the annual volatility is computed by
+    computing the standard deviation of the returns distribution.
+
+    Parameters
+    ----------
+    df_rets : pandas.Series
+        Series of simple returns of an algorithm or stock.
+    samples : int (optional)
+        Number of posterior samples to draw.
+
+    Returns
+    -------
+    pymc3.sampling.BaseTrace object
+        A PyMC3 trace object that contains samples for each parameter
+        of the posterior.
+
+    """
+
     with pm.Model() as model:
         mu = pm.Normal('mean returns', mu=0, sd=.01, testval=data.mean())
         sigma = pm.HalfCauchy('volatility', beta=1, testval=data.std())
@@ -90,6 +166,25 @@ def model_returns_t(data, samples=500):
 
 
 def compute_bayes_cone(preds, starting_value=1.):
+    """Compute 5, 25, 75 and 95 percentiles of cumulative returns, used
+    for the Bayesian cone.
+
+    Parameters
+    ----------
+    preds : numpy.array
+        Multiple (simulated) cumulative returns.
+    starting_value : int (optional)
+        Have cumulative returns start around this value.
+        Default = 1.
+
+    Returns
+    -------
+    dict of percentiles over time
+        Dictionary mapping percentiles (5, 25, 75, 95) to a
+        timeseries.
+
+    """
+
     cum_preds = np.cumprod(preds + 1, 1) * starting_value
     scoreatpercentile = lambda cum_preds, p: [stats.scoreatpercentile(c, p) for c in cum_preds.T]
     perc = {p: scoreatpercentile(cum_preds, p) for p in (5, 25, 75, 95)}
@@ -97,6 +192,22 @@ def compute_bayes_cone(preds, starting_value=1.):
     return perc
 
 def compute_consistency_score(df_test, preds):
+    """Compute Bayesian consistency score.
+
+    Parameters
+    ----------
+    df_test : pd.Series
+        Observed cumulative returns.
+    preds : numpy.array
+        Multiple (simulated) cumulative returns.
+
+    Returns
+    -------
+    Consistency score
+        Score from 100 (df_test perfectly on the median line of the
+        Bayesian cone spanned by preds) to 0 (df_test completely
+        outside of Bayesian cone.)
+    """
     df_test_cum = cum_returns(df_test, starting_value=1.)
     cum_preds = np.cumprod(preds + 1, 1)
 
@@ -137,7 +248,30 @@ def _plot_bayes_cone(df_train, df_test, preds, plot_train_len=None, ax=None):
     return ax
 
 def run_model(model, df_train, df_test=None, bmark=None, samples=500):
+    """Run one of the Bayesian models.
 
+    Parameters
+    ----------
+    model : {'alpha_beta', 't', 'normal'}
+        Which model to run
+    df_train : pd.Series
+        Timeseries of simple returns
+    df_test : pd.Series (optional)
+        Out-of-sample returns. Datetimes in df_test will be added to
+        df_train as missing values and predictions will be generated
+        for them.
+    bmark : pd.Series (optional)
+        Only used for alpha_beta to estimate regression coefficients.
+        If bmark has more recent returns than df_train, these dates
+        will be treated as missing values and predictions will be
+        generated for them taking market correlations into account.
+
+    Returns
+    -------
+    pymc3.sampling.BaseTrace object
+        A PyMC3 trace object that contains samples for each parameter
+        of the posterior.
+    """
     if model == 'alpha_beta':
        trace = model_returns_t_alpha_beta(df_train, bmark, samples)
     elif model == 't':
@@ -154,6 +288,42 @@ def run_model(model, df_train, df_test=None, bmark=None, samples=500):
 def plot_bayes_cone(df_train, df_test, bmark=None, model='t',
                     trace=None, plot_train_len=50, ax=None,
                     samples=500):
+    """Generate cumulative returns plot with Bayesian cone.
+
+    Parameters
+    ----------
+    df_train : pd.Series
+        Timeseries of simple returns
+    df_test : pd.Series
+        Out-of-sample returns. Datetimes in df_test will be added to
+        df_train as missing values and predictions will be generated
+        for them.
+    bmark : pd.Series (optional)
+        Only used for alpha_beta to estimate regression coefficients.
+        If bmark has more recent returns than df_train, these dates
+        will be treated as missing values and predictions will be
+        generated for them taking market correlations into account.
+    model : {None, 'alpha_beta', 't', 'normal'} (optional)
+        Which model to run. If none, assume trace is being passed in.
+    trace : pymc3.sampling.BaseTrace (optional)
+        Trace of a previously run model.
+    plot_train_len : int (optional)
+        How many data points to plot of df_train. Useful to zoom in on
+        the prediction if there is a long backtest period.
+    ax : matplotlib.Axis (optional)
+        Axes upon which to plot.
+    samples : int (optional)
+        Number of posterior samples to draw.
+
+    Returns
+    -------
+    score : float
+        Consistency score (see compute_consistency_score)
+    trace : pymc3.sampling.BaseTrace
+        A PyMC3 trace object that contains samples for each parameter
+        of the posterior.
+    """
+
     # generate cone
     if trace is None:
         trace = run_model(model, df_train, df_test=df_test,
