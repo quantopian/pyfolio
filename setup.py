9dde07574ede3f07a01539b6e7dbe66fb5e2f10d
Gus Gordon <gus@quantopian.com>
2015-07-13 21:04:49 +0000

    MAINT Updated requirements.

--- setup.py
+++ setup.py
@@ -27,10 +27,9 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: Scientific/Engineering :: Mathematics',
                'Operating System :: OS Independent']
 
-install_reqs = ['numpy>=1.9.2', 'scipy>=0.13.0', 'matplotlib>=1.2.1',
-                'seaborn>=0.5.1', 'pandas>=0.16.1', 'statsmodels>=0.6.1']
+install_reqs = ['Theano>=0.7.0', 'funcsigs>=0.4', 'matplotlib>=1.4.3', 'mock>=1.1.2', 'numpy>=1.9.2', 'pandas>=0.16.2', 'patsy>=0.3.0', 'pbr>=1.3.0', 'pyfolio>=0.1.beta', 'pymc3>=3.0', 'pyparsing>=2.0.3', 'python-dateutil>=2.4.2', 'pytz>=2015.4', 'scikit-learn>=0.16.1', 'scipy>=0.15.1', 'seaborn>=0.6.0', 'six>=1.9.0', 'statsmodels>=0.6.1', 'wsgiref>=0.1.2']
 
-test_reqs = ['nose']
+test_reqs = ['nose>=1.3.7', 'nose-parameterized>=0.5.0']
 
 if __name__ == "__main__":
     setup(name=DISTNAME,
