153fd70faf0ee94c1b17f09db6054a5dab2352dc
Thomas Wiecki <thomas.wiecki@gmail.com>
2015-07-27 18:41:25 +0200

    DOC Add doc string for context().

--- pyfolio/plotting.py
+++ pyfolio/plotting.py
@@ -46,6 +46,39 @@ def plotting_context(func):
 
 
 def context(context='notebook', font_scale=1.5, rc=None):
+    """Create pyfolio default plotting style context.
+
+    Under the hood, calls and returns seaborn.plotting_context() with
+    some custom settings. Usually you would use in a with-context.
+
+    Parameters
+    ----------
+    context : str, optional
+        Name of seaborn context.
+    font_scale : float, optional
+        Scale font by factor font_scale.
+    rc : dict, optional
+        Config flags.
+        By default, {'lines.linewidth': 1.5,
+                     'axes.facecolor': '0.995',
+                     'figure.facecolor': '0.97'}
+        is being used and will be added to any
+        rc passed in, unless explicitly overriden.
+
+    Returns
+    -------
+    seaborn plotting context
+
+    Example
+    -------
+    >>> with pyfolio.plotting.context(font_scale=2):
+    >>>    pyfolio.create_full_tear_sheet()
+
+    See also
+    --------
+    For more information, see seaborn.plotting_context().
+
+"""
     if rc is None:
         rc = {}
 
