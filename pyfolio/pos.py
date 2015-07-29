55fd17746ca82b847d1ceef66f2444ba3cd5fa67
David Edwards <dedwards@quantopian.com>
2015-07-29 14:33:32 -0400

    [BUG] Fixes a deleted .max() that was necessary
    Accidentally deleted a .max() from get_top_long_short_abs,
    it has been replaced.

--- pyfolio/pos.py
+++ pyfolio/pos.py
@@ -96,7 +96,7 @@ def get_top_long_short_abs(positions, top=10):
     positions = positions.drop('cash', axis='columns')
     df_max = positions.max()
     df_min = positions.min()
-    df_abs_max = positions.abs()
+    df_abs_max = positions.abs().max()
     df_top_long = df_max[df_max > 0].nlargest(top)
     df_top_short = df_min[df_min < 0].nsmallest(top)
     df_top_abs = df_abs_max.nlargest(top)
