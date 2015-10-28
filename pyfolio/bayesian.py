551bafe750c92c7687e9dd529e07d53b67d57f8b
Shane Bussmann <sbussmann@gmail.com>
2015-10-28 02:15:28 -0400

    Still a work in progress
    I think this generalization of alpha_beta to regress on multiple betas
    should work, but I haven’t tested it at all yet.  Will do so in a few
    hours.

--- pyfolio/bayesian.py
+++ pyfolio/bayesian.py
@@ -35,7 +35,7 @@ def model_returns_t_alpha_beta(data, bmark, samples=2000):
     benchmark returns (e.g. S&P500). The data is assumed to be T
     distributed and thus is robust to outliers and takes tail events
     into account.  If a pandas.DataFrame is passed as a benchmark, then
-    multilinear regression is used to estimate alpha and beta.
+    multiple linear regression is used to estimate alpha and beta.
 
     Parameters
     ----------
@@ -58,15 +58,12 @@ def model_returns_t_alpha_beta(data, bmark, samples=2000):
         of the posterior.
     """
 
-    # check whether the benchmark is a Series or DataFrame
-    if bmark.ndim == 1:
-        bmark_is_series = True
+    bmark = bmark.dropna()
 
-    import pdb; pdb.set_trace()
     if data.shape[0] != bmark.shape[0]:
         data = pd.Series(data, index=bmark.index)
 
-
+    Nbmark = bmark.shape[1]
     data_no_missing = data.dropna()
 
     with pm.Model():
@@ -78,7 +75,8 @@ def model_returns_t_alpha_beta(data, bmark, samples=2000):
 
         # alpha and beta
         import pdb; pdb.set_trace()
-        beta_init, alpha_init = np.linalg.lstsq(
+        #beta_init, alpha_init = np.linalg.lstsq(
+        alphabeta_init = np.linalg.lstsq(
             bmark.loc[data_no_missing.index],
             data_no_missing)#[:2]
 
@@ -86,12 +84,22 @@ def model_returns_t_alpha_beta(data, bmark, samples=2000):
         #    bmark.loc[data_no_missing.index],
         #    data_no_missing)[:2]
 
-        alpha_reg = pm.Normal('alpha', mu=0, sd=.1, testval=alpha_init)
-        beta_reg = pm.Normal('beta', mu=0, sd=1, testval=beta_init)
+        ab_reg = []
+        abi_string = ['alpha'] + ['beta'] * Nbmark
+        abi_sd = [0.1] + [1.0] * Nbmark
+        for i, abi in enumerate(alphabeta_init):
+            ab_reg.append(pm.Normal(abi_string[i], mu=0, sd=abi_sd[i], 
+                testval=abi))
+
+        #alpha_reg = pm.Normal('alpha', mu=0, sd=.1, testval=alpha_init)
+        #beta_reg = pm.Normal('beta', mu=0, sd=1, testval=beta_init)
+        mu_reg = ab_reg[0]
+        for i in range(Nbmark):
+            mu_reg += ab_reg[i] * bmark.ix[data_no_missing.index, i]
 
         pm.T('returns',
              nu=nu + 2,
-             mu=alpha_reg + beta_reg * bmark,
+             mu=mu_reg,
              sd=sigma,
              observed=data)
         start = pm.find_MAP(fmin=sp.optimize.fmin_powell)
