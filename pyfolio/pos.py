cd21298288d4b413ac01902768433fcb5a0f1662
gusgordon <gordgus@quantopian.com>
2016-11-23 18:44:32 +0000

    Fix exposure calculation and update related plotting functions

--- pyfolio/pos.py
+++ pyfolio/pos.py
@@ -40,34 +40,6 @@ def get_percent_alloc(values):
     )
 
 
-def get_long_short_pos(positions):
-    """
-    Determines the long and short allocations in a portfolio.
-
-    Parameters
-    ----------
-    positions : pd.DataFrame
-        The positions that the strategy takes over time.
-
-    Returns
-    -------
-    df_long_short : pd.DataFrame
-        Long and short allocations as a decimal
-        percentage of the total net liquidation
-    """
-
-    pos_wo_cash = positions.drop('cash', axis=1)
-    longs = pos_wo_cash[pos_wo_cash > 0].sum(axis=1).fillna(0)
-    shorts = pos_wo_cash[pos_wo_cash < 0].sum(axis=1).fillna(0)
-    cash = positions.cash
-    net_liquidation = longs - shorts + cash
-    df_pos = pd.DataFrame({'long': longs.divide(net_liquidation, axis='index'),
-                           'short': shorts.divide(net_liquidation,
-                                                  axis='index')})
-    df_pos['net exposure'] = df_pos['long'] + df_pos['short']
-    return df_pos
-
-
 def get_top_long_short_abs(positions, top=10):
     """
     Finds the top long, short, and absolute positions.
