9a5b57ea220d489cb0d20368c8e4168a8a8aab17
gusgordon <gordgus@quantopian.com>
2016-12-07 21:18:49 +0000

    Use new version of empyrical

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ install_reqs = [
     'scipy>=0.14.0',
     'seaborn>=0.7.1',
     'pandas-datareader>=0.2',
-    'empyrical>=0.2.1'
+    'empyrical>=0.2.2'
 ]
 
 test_reqs = ['nose>=1.3.7', 'nose-parameterized>=0.5.0', 'runipy>=0.1.3']
