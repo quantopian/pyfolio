ce6e13adbaf9b182b785c98402283931bc695104
Henning Kopp <henning.kopp@uni-ulm.de>
2017-05-27 14:33:19 +0200

    added running_volatility timeseries

--- pyfolio/timeseries.py
+++ pyfolio/timeseries.py
@@ -1024,6 +1024,28 @@ def gen_drawdown_table(returns, top=10):
     return df_drawdowns
 
 
+def rolling_volatility(returns, rolling_vol_window):
+    """
+    Determines the rolling volatility of a strategy.
+
+    Parameters
+    ----------
+    returns : pd.Series
+        Daily returns of the strategy, noncumulative.
+         - See full explanation in tears.create_full_tear_sheet.
+    rolling_vol_window : int
+        Length of rolling window, in days, over which to compute.
+
+    Returns
+    -------
+    pd.Series
+        Rolling volatility.
+    """
+
+    return returns.rolling(rolling_vol_window).std() \
+        * np.sqrt(APPROX_BDAYS_PER_YEAR)
+
+
 def rolling_sharpe(returns, rolling_sharpe_window):
     """
     Determines the rolling Sharpe ratio of a strategy.
