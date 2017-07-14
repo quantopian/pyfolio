ab60891e16c7feb48bf1755232f06ae8ab8116ab
hkopp <henning.kopp@uni-ulm.de>
2017-07-14 10:44:57 +0200

    fixes #278 (#390)
    * removed trailing whitespace
    
    * PEP8 policing
    
    * fixes #368
    
    * added running_volatility timeseries
    
    * added plot for running_volatility
    
    * added running volatility in returns_tear_sheet
    
    * pep8 policing
    
    * plot rolling vol against benchmark
    
    * fixed default factor returns
    
    * matching quotes
    
    * explicit use of keyword

--- pyfolio/plotting.py
+++ pyfolio/plotting.py
@@ -896,6 +896,70 @@ def plot_rolling_beta(returns, factor_returns, legend_loc='best',
     return ax
 
 
+def plot_rolling_volatility(returns, factor_returns=None,
+                            rolling_window=APPROX_BDAYS_PER_MONTH * 6,
+                            legend_loc='best', ax=None, **kwargs):
+    """
+    Plots the rolling volatility versus date.
+
+    Parameters
+    ----------
+    returns : pd.Series
+        Daily returns of the strategy, noncumulative.
+         - See full explanation in tears.create_full_tear_sheet.
+    factor_returns : pd.Series, optional
+        Daily noncumulative returns of the benchmark.
+    rolling_window : int, optional
+        The days window over which to compute the volatility.
+    legend_loc : matplotlib.loc, optional
+        The location of the legend on the plot.
+    ax : matplotlib.Axes, optional
+        Axes upon which to plot.
+    **kwargs, optional
+        Passed to plotting function.
+
+    Returns
+    -------
+    ax : matplotlib.Axes
+        The axes that were plotted on.
+    """
+
+    if ax is None:
+        ax = plt.gca()
+
+    y_axis_formatter = FuncFormatter(utils.two_dec_places)
+    ax.yaxis.set_major_formatter(FuncFormatter(y_axis_formatter))
+
+    rolling_vol_ts = timeseries.rolling_volatility(
+        returns, rolling_window)
+    rolling_vol_ts.plot(alpha=.7, lw=3, color='orangered', ax=ax,
+                        **kwargs)
+    if factor_returns is not None:
+        rolling_vol_ts_factor = timeseries.rolling_volatility(
+            factor_returns, rolling_window)
+        rolling_vol_ts_factor.plot(alpha=.7, lw=3, color='grey', ax=ax,
+                                   **kwargs)
+
+    ax.set_title('Rolling Volatility (6-month)')
+    ax.axhline(
+        rolling_vol_ts.mean(),
+        color='orangered',
+        linestyle='--',
+        lw=3)
+
+    ax.axhline(0.0, color='black', linestyle='-', lw=2)
+
+    ax.set_ylabel('Volatility')
+    ax.set_xlabel('')
+    if factor_returns.empty:
+        ax.legend(['Volatility', 'Average Volatility'],
+                  loc=legend_loc)
+    else:
+        ax.legend(['Volatility', 'Benchmark Volatility', 'Average Volatility'],
+                  loc=legend_loc)
+    return ax
+
+
 def plot_rolling_sharpe(returns, rolling_window=APPROX_BDAYS_PER_MONTH * 6,
                         legend_loc='best', ax=None, **kwargs):
     """
