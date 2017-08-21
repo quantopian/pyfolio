c6de2978c56d9609a3cfe41bc3d7d2f5c8f62fcb
George Ho <georgh0021@gmail.com>
2017-08-21 22:04:55 +0800

    MAINT removed plot_monthly_returns_timeseries

--- pyfolio/plotting.py
+++ pyfolio/plotting.py
@@ -1739,62 +1739,6 @@ def show_worst_drawdown_periods(returns, top=5):
                       name='Worst drawdown periods', fmt='{0:.2f}')
 
 
-def plot_monthly_returns_timeseries(returns, ax=None, **kwargs):
-    """
-    Plots monthly returns as a timeseries.
-
-    Parameters
-    ----------
-    returns : pd.Series
-        Daily returns of the strategy, noncumulative.
-         - See full explanation in tears.create_full_tear_sheet.
-    ax : matplotlib.Axes, optional
-        Axes upon which to plot.
-    **kwargs, optional
-        Passed to seaborn plotting function.
-
-    Returns
-    -------
-    ax : matplotlib.Axes
-        The axes that were plotted on.
-    """
-
-    def cumulate_returns(x):
-        return empyrical.cum_returns(x)[-1]
-
-    if ax is None:
-        ax = plt.gca()
-
-    monthly_rets = returns.resample('M').apply(lambda x: cumulate_returns(x))
-    monthly_rets = monthly_rets.to_period()
-
-    sns.barplot(x=monthly_rets.index,
-                y=monthly_rets.values,
-                color='steelblue')
-
-    locs, labels = plt.xticks()
-    plt.setp(labels, rotation=90)
-
-    # only show x-labels on year boundary
-    xticks_coord = []
-    xticks_label = []
-    count = 0
-    for i in monthly_rets.index:
-        if i.month == 1:
-            xticks_label.append(i)
-            xticks_coord.append(count)
-            # plot yearly boundary line
-            ax.axvline(count, color='gray', ls='--', alpha=0.3)
-
-        count += 1
-
-    ax.axhline(0.0, color='darkgray', ls='-')
-    ax.set_xticks(xticks_coord)
-    ax.set_xticklabels(xticks_label)
-
-    return ax
-
-
 def plot_round_trip_lifetimes(round_trips, disp_amount=16, lsize=18, ax=None):
     """
     Plots timespans and directions of a sample of round trip trades.
