e3326860ea1fb121584bab484978dc028c2617d1
Vikram Narayan <vnarayan@quantopian.com>
2017-08-22 08:45:37 -0400

    ENH: add perf attribution + summary table (#417)
    * ENH: add perf attribution + summary table
    
    * STY: flake8
    
    * MAINT: fix inputs, use empyrical for calculations
    
    * MAINT: use specifc_returns for perf_attrib_stats
    
    * TST: test perf_attrib simple case
    
    * MAINT: update docstring, flake8
    
    * MAINT: del cash col after normalizing positions
    
    * TST: add long/short perf attrib test
    
    * MAINT: remove unused test stuff
    
    * MAINT: update docstrings, use pd.utils for tests

---
+++ pyfolio/perf_attrib.py
@@ -0,0 +1,138 @@
+#
+# Copyright 2017 Quantopian, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import division
+import pandas as pd
+
+import empyrical
+from pyfolio.pos import get_percent_alloc
+from pyfolio.utils import print_table
+
+
+def perf_attrib(returns, positions, factor_returns, factor_loadings):
+    """
+    Does performance attribution given risk info.
+
+    Parameters
+    ----------
+    returns : pd.Series
+        Returns for each day in the date range.
+        - Example:
+            2017-01-01   -0.017098
+            2017-01-02    0.002683
+            2017-01-03   -0.008669
+
+    positions: pd.DataFrame
+        Daily holdings (in dollars or percentages), indexed by date.
+        Will be converted to percentages if positions are in dollars.
+        Short positions show up as cash in the 'cash' column.
+        - Examples:
+                        AAPL  TLT  XOM  cash
+            2017-01-01    34   58   10     0
+            2017-01-02    22   77   18     0
+            2017-01-03   -15   27   30    15
+
+                            AAPL       TLT       XOM  cash
+            2017-01-01  0.333333  0.568627  0.098039   0.0
+            2017-01-02  0.188034  0.658120  0.153846   0.0
+            2017-01-03  0.208333  0.375000  0.416667   0.0
+
+    factor_returns : pd.DataFrame
+        Returns by factor, with date as index and factors as columns
+        - Example:
+                        momentum  reversal
+            2017-01-01  0.002779 -0.005453
+            2017-01-02  0.001096  0.010290
+
+    factor_loadings : pd.DataFrame
+        Factor loadings for all days in the date range, with date and ticker as
+        index, and factors as columns.
+        - Example:
+                               momentum  reversal
+            dt         ticker
+            2017-01-01 AAPL   -1.592914  0.852830
+                       TLT     0.184864  0.895534
+                       XOM     0.993160  1.149353
+            2017-01-02 AAPL   -0.140009 -0.524952
+                       TLT    -1.066978  0.185435
+                       XOM    -1.798401  0.761549
+
+    Returns
+    -------
+    perf_attribution : pd.DataFrame
+        df with factors, common returns, and specific returns as columns,
+        and datetimes as index
+        - Example:
+                        momentum  reversal  common_returns  specific_returns
+            dt
+            2017-01-01  0.249087  0.935925        1.185012          1.185012
+            2017-01-02 -0.003194 -0.400786       -0.403980         -0.403980
+    """
+    # convert holdings to percentages, and convert positions to long format
+    positions = get_percent_alloc(positions)
+    # remove cash after normalizing positions
+    del positions['cash']
+    positions = positions.stack()
+    positions.index = positions.index.set_names(['dt', 'ticker'])
+
+    risk_exposures = factor_loadings.multiply(positions,
+                                              axis='rows')
+
+    risk_exposures_portfolio = risk_exposures.groupby(level='dt').sum()
+    perf_attrib_by_factor = risk_exposures_portfolio.multiply(factor_returns)
+
+    common_returns = perf_attrib_by_factor.sum(axis='columns')
+    specific_returns = returns - common_returns
+
+    returns_df = pd.DataFrame({'total_returns': returns,
+                               'common_returns': common_returns,
+                               'specific_returns': specific_returns})
+
+    return pd.concat([perf_attrib_by_factor, returns_df], axis='columns')
+
+
+def create_perf_attrib_stats(perf_attrib):
+    """
+    Takes perf attribution data over a period of time and computes annualized
+    multifactor alpha, multifactor sharpe, risk exposures.
+    """
+    summary = {}
+    specific_returns = perf_attrib['specific_returns']
+    common_returns = perf_attrib['common_returns']
+
+    summary['Annual multi-factor alpha'] =\
+        empyrical.annual_return(specific_returns)
+
+    summary['Multi-factor sharpe'] =\
+        empyrical.sharpe_ratio(specific_returns)
+
+    summary['Cumulative specific returns'] =\
+        empyrical.cum_returns(specific_returns)
+    summary['Cumulative common returns'] =\
+        empyrical.cum_returns(common_returns)
+    summary['Total returns'] =\
+        empyrical.cum_returns(perf_attrib['total_returns'])
+
+    summary = pd.Series(summary)
+    return summary
+
+
+def show_perf_attrib_stats(perf_attrib_data, risk_exposures):
+    """
+    Takes perf attribution data over a period of time, computes stats on it,
+    and displays them using `utils.print_table`.
+    """
+    perf_attrib_stats = create_perf_attrib_stats(perf_attrib_data)
+    print_table(perf_attrib_stats)
+    print_table(risk_exposures)
---
+++ pyfolio/tests/test_perf_attrib.py
@@ -0,0 +1,122 @@
+import numpy as np
+import pandas as pd
+import unittest
+
+from pyfolio.perf_attrib import perf_attrib
+
+
+def generate_toy_risk_model_output():
+    """
+    Generate toy risk model output.
+
+    Returns
+    -------
+    tuple of (returns, factor_returns, positions, factor_loadings)
+    returns : pd.DataFrame
+    factor_returns : pd.DataFrame
+    """
+    start_date = '2017-01-01'
+    periods = 10
+    dts = pd.date_range(start_date, periods=periods)
+    np.random.seed(123)
+    tickers = ['AAPL', 'TLT', 'XOM', 'cash']
+    styles = ['momentum', 'reversal']
+
+    returns = pd.Series(index=dts,
+                        data=np.random.randn(10)) / 100
+
+    factor_returns = pd.DataFrame(
+        columns=styles, index=dts,
+        data=np.random.randn(periods, len(styles))) / 100
+
+    arrays = [dts, tickers]
+    index = pd.MultiIndex.from_product(arrays, names=['dt', 'ticker'])
+
+    positions = pd.DataFrame(
+        columns=tickers, index=dts,
+        data=np.random.randint(100, size=(10, len(tickers)))
+    )
+    positions['cash'] = positions.sum(axis=1)
+
+    factor_loadings = pd.DataFrame(columns=['factor1', 'factor2'],
+                                   index=index,
+                                   data=np.random.randn(30, 2))
+
+    return returns, positions, factor_returns, factor_loadings
+
+
+class PerfAttribTestCase(unittest.TestCase):
+
+    def test_perf_attrib_simple(self):
+
+        start_date = '2017-01-01'
+        periods = 2
+        dts = pd.date_range(start_date, periods=periods)
+        dts.name = 'dt'
+
+        tickers = ['stock1', 'stock2']
+        styles = ['risk_factor1', 'risk_factor2']
+
+        returns = pd.Series(data=[0.1, 0.1], index=dts)
+
+        factor_returns = pd.DataFrame(
+            columns=styles,
+            index=dts,
+            data={'risk_factor1': [.1, .1],
+                  'risk_factor2': [.1, .1]}
+        )
+
+        positions = pd.DataFrame(
+            index=dts,
+            data={'stock1': [20, 20],
+                  'stock2': [50, 50],
+                  'cash': [0, 0]}
+        )
+
+        index = pd.MultiIndex.from_product(
+            [dts, tickers], names=['dt', 'ticker'])
+
+        factor_loadings = pd.DataFrame(
+            columns=styles,
+            index=index,
+            data={'risk_factor1': [0.25, 0.25, 0.25, 0.25],
+                  'risk_factor2': [0.25, 0.25, 0.25, 0.25]}
+        )
+
+        expected_perf_attrib_output = pd.DataFrame(
+            index=dts,
+            columns=['risk_factor1', 'risk_factor2', 'common_returns',
+                     'specific_returns', 'total_returns'],
+            data={'risk_factor1': [0.025, 0.025],
+                  'risk_factor2': [0.025, 0.025],
+                  'common_returns': [0.05, 0.05],
+                  'specific_returns': [0.05, 0.05],
+                  'total_returns': returns}
+        )
+
+        perf_attrib_output = perf_attrib(returns, positions,
+                                         factor_returns, factor_loadings)
+
+        pd.util.testing.assert_frame_equal(expected_perf_attrib_output,
+                                           perf_attrib_output)
+
+        # test long and short positions
+        positions = pd.DataFrame(index=dts,
+                                 data={'stock1': [20, 20],
+                                       'stock2': [-20, -20],
+                                       'cash': [20, 20]})
+
+        perf_attrib_output = perf_attrib(returns, positions,
+                                         factor_returns, factor_loadings)
+        expected_perf_attrib_output = pd.DataFrame(
+            index=dts,
+            columns=['risk_factor1', 'risk_factor2', 'common_returns',
+                     'specific_returns', 'total_returns'],
+            data={'risk_factor1': [0.0, 0.0],
+                  'risk_factor2': [0.0, 0.0],
+                  'common_returns': [0.0, 0.0],
+                  'specific_returns': [0.1, 0.1],
+                  'total_returns': returns}
+        )
+        pd.util.testing.assert_frame_equal(expected_perf_attrib_output,
+                                           perf_attrib_output)
