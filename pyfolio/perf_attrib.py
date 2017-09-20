367ed742c69edaa15301de2ffe05df94fcc61aba
vikram-narayan <vnarayan@quantopian.com>
2017-09-20 17:32:10 -0400

    ENH: add cost plotting to plot_returns

--- pyfolio/perf_attrib.py
+++ pyfolio/perf_attrib.py
@@ -176,7 +176,7 @@ def show_perf_attrib_stats(returns, positions, factor_returns,
     print_table(risk_exposure_stats)
 
 
-def plot_returns(perf_attrib_data, ax=None):
+def plot_returns(perf_attrib_data, cost=None, ax=None):
     """
     Plot total, specific, and common returns.
 
@@ -184,12 +184,17 @@ def plot_returns(perf_attrib_data, ax=None):
     ----------
     perf_attrib_data : pd.DataFrame
         df with factors, common returns, and specific returns as columns,
-        and datetimes as index
+        and datetimes as index. Assumes the `total_returns` column is NOT
+        cost adjusted.
         - Example:
                         momentum  reversal  common_returns  specific_returns
             dt
             2017-01-01  0.249087  0.935925        1.185012          1.185012
             2017-01-02 -0.003194 -0.400786       -0.403980         -0.403980
+
+    cost : pd.Series, optional
+        if present, gets subtracted from `perf_attrib_data['total_returns']`,
+        and gets plotted separately
 
     ax :  matplotlib.axes.Axes
         axes on which plots are made. if None, current axes will be used
@@ -198,18 +203,28 @@ def plot_returns(perf_attrib_data, ax=None):
     -------
     ax :  matplotlib.axes.Axes
     """
+
     if ax is None:
         ax = plt.gca()
 
     returns = perf_attrib_data['total_returns']
+    total_returns_label = 'Total returns'
+
+    if cost is not None:
+        returns = returns - cost
+        total_returns_label += ' (adjusted)'
+
     specific_returns = perf_attrib_data['specific_returns']
     common_returns = perf_attrib_data['common_returns']
 
-    ax.plot(ep.cum_returns(returns), color='g', label='Total returns')
+    ax.plot(ep.cum_returns(returns), color='g', label=total_returns_label)
     ax.plot(ep.cum_returns(specific_returns), color='b',
             label='Cumulative specific returns')
     ax.plot(ep.cum_returns(common_returns), color='r',
             label='Cumulative common returns')
+
+    if cost is not None:
+        ax.plot(cost, color='p', label='Cost')
 
     ax.set_title('Time series of cumulative returns')
     ax.set_ylabel('Returns')
