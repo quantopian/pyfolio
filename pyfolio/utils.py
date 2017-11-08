70d45768fb092b65038f25cf95209b127a98a2ed
vikram-narayan <vnarayan@quantopian.com>
2017-11-08 15:36:22 -0500

    MAINT: remove some colors from color set

--- pyfolio/utils.py
+++ pyfolio/utils.py
@@ -57,9 +57,8 @@ DEPRECATION_WARNING = ("Data loaders have been moved to empyrical and will "
 COLORMAP = 'Paired'
 COLORS = ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231',
           '#911eb4', '#46f0f0', '#f032e6', '#d2f53c', '#fabebe',
-          '#008080', '#e6beff', '#aa6e28', '#fffac8', '#800000',
-          '#aaffc3', '#808000', '#ffd8b1', '#000080', '#808080',
-          '#FFFFFF', '#000000']
+          '#008080', '#e6beff', '#aa6e28', '#800000', '#aaffc3',
+          '#808000', '#ffd8b1', '#000080', '#808080']
 
 
 def one_dec_places(x, pos):
