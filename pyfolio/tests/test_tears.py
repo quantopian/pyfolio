5a749ecbeea97512351e6b63e9c24ebc50e2cf62
George Ho <georgeho1618@gmail.com>
2018-05-21 12:12:40 -0400

    TST: fixed tests

--- pyfolio/tests/test_tears.py
+++ pyfolio/tests/test_tears.py
@@ -49,6 +49,7 @@ class PositionsTestCase(TestCase):
         create_full_tear_sheet(self.test_returns,
                                positions=self.test_pos,
                                transactions=self.test_txn,
+                               benchmark_rets=self.test_returns,
                                **kwargs
                                )
 
@@ -73,6 +74,7 @@ class PositionsTestCase(TestCase):
     @cleanup
     def test_create_returns_tear_sheet_breakdown(self, kwargs):
         create_returns_tear_sheet(self.test_returns,
+                                  benchmark_rets=self.test_returns,
                                   **kwargs
                                   )
 
