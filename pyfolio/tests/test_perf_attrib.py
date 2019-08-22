b358c87fb3fa415a92bee85449902faa43e1289f
Thomas Wiecki <thomas.wiecki@gmail.com>
2019-08-22 09:26:38 +0200

    Add tilt and timing to perf_attrib test.

--- pyfolio/tests/test_perf_attrib.py
+++ pyfolio/tests/test_perf_attrib.py
@@ -161,11 +161,14 @@ class PerfAttribTestCase(unittest.TestCase):
         expected_perf_attrib_output = pd.DataFrame(
             index=dts,
             columns=['risk_factor1', 'risk_factor2', 'total_returns',
-                     'common_returns', 'specific_returns'],
+                     'common_returns', 'specific_returns',
+                     'tilt_returns', 'timing_returns'],
             data={'risk_factor1': [0.0, 0.0],
                   'risk_factor2': [0.0, 0.0],
                   'common_returns': [0.0, 0.0],
                   'specific_returns': [0.1, 0.1],
+                  'tilt_returns': [0.0, 0.0],
+                  'timing_returns': [0.0, 0.0],
                   'total_returns': returns}
         )
 
